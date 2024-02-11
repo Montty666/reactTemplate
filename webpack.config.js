0c2de45a991735e39ca131bf0f458fb4127e5a99
unknown <montty666@gmail.com>
2024-02-12 01:21:37 +0700

    init repo

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: path.join(__dirname, 'src', 'index.tsx')
+    },
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'src'),
+        },
+        compress: true,
+        port: 9000,
+      },
+    target: 'web',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: '/node_modules/'
+            }
+        ],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html')
+        })
+    ]
+}
